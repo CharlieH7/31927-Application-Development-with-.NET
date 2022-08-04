111944f6ce72edaab82bd55e4681ba7471651eea
Charlie Huang <hope91824@gmail.com>
2022-08-04 12:14:45 +1000

    Add week 1 lab

---
+++ Week 1 - Introduction to Application Development with .NET and .NET Overview/Tutorial-Lab1-ProgrammingQuestions/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Write a program to print Hello World 
+in the console
+ */
+namespace Week1LabProgram
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            // Write the code to print Hello World
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
+
+/*
+Test case:
+
+Expected Output:
+Hello World
+
+*/
