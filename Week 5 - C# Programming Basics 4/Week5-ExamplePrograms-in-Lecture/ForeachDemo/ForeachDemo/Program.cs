edc9d83cb8d4e37300c0f29081729c936f5fd7ba
Charlie Huang <hope91824@gmail.com>
2022-09-12 14:51:19 +1000

    add week 5 content

---
+++ Week 5 - C# Programming Basics 4/Week5-ExamplePrograms-in-Lecture/ForeachDemo/ForeachDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+//Foreach demo
+
+namespace Week5ClassProgram
+{
+    class ForeachDemo
+    {
+        static void Main(string[] args)
+        {
+            // Create an ArrayList
+            ArrayList myArraylist = new ArrayList();
+
+            // Add data into the arrayList
+            for (int loopVar = 10; loopVar<15; loopVar++)
+            {
+                myArraylist.Add(loopVar);
+            }
+
+            // Display the elements in the ArrayList
+            Console.WriteLine("Elements in the arrayList are:");
+            foreach (int element in myArraylist)
+            {
+                Console.Write(element + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
