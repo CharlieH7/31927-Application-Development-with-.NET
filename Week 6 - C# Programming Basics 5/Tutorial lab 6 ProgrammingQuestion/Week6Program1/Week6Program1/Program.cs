2a42a4824c619133b9b0c9b5b18e1bca2f0ce4ae
Charlie Huang <hope91824@gmail.com>
2022-09-21 16:22:59 +1000

    week 6 lab

---
+++ Week 6 - C# Programming Basics 5/Tutorial lab 6 ProgrammingQuestion/Week6Program1/Week6Program1/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+/*
+Write a program to Create Student a Result management System with the following specifications:
+1. There are two classes, Person (Abstract base class), Student(derived class from person)
+2. There is one Interface for Generating the Results
+
+Please refer to the instructions document for the class diagrams and Instructions.
+
+    */
+
+namespace Week6LabProgram
+{
+    // Create an Abstract class person
+    public abstract class Person
+    {
+        //Write code to Declare data members/properties here
+        public string personName { get; set; }
+        public string personAddress { get; set; }
+        // Write code for Constructor
+        public Person(string personName, string personAddress)
+        {
+            this.personName = personName;
+            this.personAddress = personAddress;
+        }
+    }
+
+    // Write code toCreate an Interface IResults for creating marksheet
+    public interface IResults
+    {
+        // Declare the methods to be inplemented in the class
+
+        // Write code for Method to get the marks for 5 subjects
+        void GetMarks();
+        // Write code for Method to calculate the final grade (Pass/Fail)
+        string CalculateResult();
+        //Write code for Method to Display the MarkSheet
+        void DisplayResult();
+    }
+
+    // Write code to Create the Student class by inheriting the Person Class and implementing the IResults interface
+    public class Student : Person, IResults
+    {
+        // Write code for Data members specific to a Student
+        private string standard;
+        private string roll;
+        private double[] marks;
+
+        // Write code for Student Constructor, which also initializes the base class properties
+        public Student(string name, string address, string standard, string roll) : base(name, address)
+        {
+            // Write code here
+            this.standard = standard;
+            this.roll = roll;
+            this.marks = new double[5];
+        }
+
+        // Implement the GetMarks() method
+        public void GetMarks()
+        {
+            // Accept the marks for 5 subjects and store it in an array
+            for (int loopVar = 0; loopVar < 5; loopVar++)
+            {
+                // Write code to to accept marks for 5 subjects
+                Console.Write("Enter Marks for Subject-{0}: ", loopVar + 1);
+                string userInput = Console.ReadLine();
+                this.marks[loopVar] = Convert.ToDouble(userInput);
+            }
+        }
+        // Implement the CalculateResult() method
+        public string CalculateResult()
+        {
+            // Calculate the sum of the marks obtained// Hint: mark.Sum()
+            double totalMarksObtained = this.marks.Sum();
+            // Find the grade and return it
+            // Averge <40, grade = Fail
+            // Else Pass
+            if ((totalMarksObtained / 5) < 40)
+                return "Fail";
+            else
+                return "Pass";
+        }
+
+        // Implement the DisplayResult() method to generate the Marksheet
+        public void DisplayResult()
+        {
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("\t\tMark Sheet");
+            Console.WriteLine("---------------------------------------------");
+            // Write code to to display the marksheet as per format given in test case
+            Console.WriteLine("Name:{0}", this.personName);
+            Console.WriteLine("Class:{0}", this.standard);
+            Console.WriteLine("Roll:{0}", this.roll);
+            Console.WriteLine("Address:{0}", this.personAddress);
+            Console.WriteLine("\nMarks Obtained:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Subject-{0}: {1}", i + 1, this.marks[i]);
+            }
+            Console.WriteLine("\nAverage Marks: {0}", this.marks.Average());
+            Console.WriteLine("Final Grade: {0}", this.CalculateResult());
+            Console.WriteLine("---------------------------------------------");
+
+        }
+        class Week6Program1
+        {
+            static void Main(string[] args)
+            {
+                // Write code to Create a Student object
+                Student s1 = new Student("Charlie Huang", "30 Ivory Street", "X", "1004"); ;
+                // Write code to Get the student's marks
+                s1.GetMarks();
+                // Write code to Generate the Marks Sheet
+                s1.DisplayResult();
+                // Accept a key press from user.
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
+/* Test Case:
+
+Enter Marks for Subject-1:56
+Enter Marks for Subject-2:42
+Enter Marks for Subject-3:89
+Enter Marks for Subject-4:69
+Enter Marks for Subject-5:95
+
+---------------------------------------------
+                Mark Sheet
+---------------------------------------------
+Name: George Woolsworth
+Class: V
+Roll: 1004
+Address: Ultimo, Sydney 2007, Australia
+
+Marks Obtained:
+Subject-0: 57
+Subject-1: 43
+Subject-2: 90
+Subject-3: 70
+Subject-4: 96
+
+Average Marks: 70.2
+Final Grade: Pass
+---------------------------------------------
+
+    
+    */
---
+++ Week 6 - C# Programming Basics 5/Tutorial lab 6 ProgrammingQuestion/Week6Program2/Week6Program2/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+/*
+Write a program to create a Shape Class and derive two child class, Rectangle and Circle, based 
+the specification given in the Tutorial instructions.
+ The Base class implementation is provided.
+ */
+namespace Week6LabProgram
+{
+    // Base Class Shape
+    public class Shape
+    {
+        // Data members/properties
+        public int NumberOfSides { get; set; }
+
+        // Constructor
+        public Shape(int NoSides)
+        {
+            NumberOfSides = NoSides;
+        }
+
+        // Virtual method Area() to be overidden by Child class
+        public virtual void Area()
+        {
+            Console.WriteLine("I am from Shape Class");
+        }
+        // Virtual method Display() to be overidden by Child class
+        public virtual void Display()
+        {
+            Console.WriteLine("The Number of Sides are: {0}", NumberOfSides);
+        }
+    }
+
+    // Create the Child class Circle derived from Shape
+    public class Circle : Shape
+    {
+        // Write the code for Data member/properties
+        private double radius;
+        private double areaVal;
+        private const double PI = Math.PI;
+
+        // Write the code for Constructor
+        public Circle(int noSides, double radius): base(noSides)
+        {
+           this.radius = radius;
+        }
+
+        // Write the code to override the base class Area() method
+        public override void Area()
+        {
+            areaVal = this.radius * this.radius * PI;
+        }
+        // Write the code to override the base class Display() method
+        public override void Display()
+        {
+            Console.WriteLine("\nThe Number of sides of a Circle is : {0}", base.NumberOfSides);
+            Console.WriteLine("The Radius of the Circle is: {0}", this.radius);
+            Console.WriteLine("The Area of Circle is : {0}", this.areaVal);
+        }
+    }
+
+    // Create the child class Rectangle derived from Shape
+    public class Rectangle : Shape
+    {
+        // Write the code for Data member/properties
+        private double height;
+        private double width;
+        private double areaVal;
+        
+        // Write the code for Constructor
+        public Rectangle(int noSides, double height, double width) : base(noSides)
+        {
+            this.height = height;
+            this.width = width;
+        }
+        // Write the code to override the base class Area() method
+        public override void Area()
+        {
+            areaVal = height * width;
+        }
+        // Write the code to override the base class Display() method
+        public override void Display()
+        {
+            //  hint: Check the Display method of the Circle class to complete the code.
+            Console.WriteLine("\nThe Number of sides of a Rectangle is : {0}", base.NumberOfSides);
+            Console.WriteLine("The Height and Width of the Rectangle is: {0}, {1}", this.height, this.width);
+            Console.WriteLine("The Area of Rectangle is : {0}", this.areaVal);
+
+        }
+    }
+    class Week6Program2
+    {
+        static void Main(string[] args)
+        {
+            // Create Circle and Rectangle Objects
+            Circle C1 = new Circle(1, 4);
+            Rectangle R1 = new Rectangle(4, 5, 4);
+
+            // Calculate the area
+            C1.Area();
+            R1.Area();
+
+            // Display the dimensions and area of Circle and Rectangle
+            C1.Display();
+            R1.Display();
+
+            Console.ReadKey();
+            
+        }
+    }
+}
+
+/*
+  Test Case:
+    The Number of sides of a Circle is : 1
+    The Radius of the Circle is: 4
+    The Area of Circle is : 50.272
+
+    The Number of sides of a Rectangle is : 4
+    The Length and Breadth of the Rectangle is: 5, 4
+    The Area of Rectangle is : 20
+  
+ */
