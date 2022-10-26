ce51bda61840ffe72e8ddbf4670c3ae694ab135a
Charlie Huang <hope91824@gmail.com>
2022-10-26 18:19:38 +1100

    Week 9 content

---
+++ Week 9 - RichTextBox, Collections and Generics/Week 9 Programs in Class/GenericDictionaryDemo/GenericDictionaryDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericDictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> weekdays = new Dictionary<int, string>();
+
+            // Add values
+            weekdays.Add(1, "Monday");
+            weekdays.Add(2, "Tuesday");
+            weekdays.Add(3, "Wednesday");
+            weekdays.Add(4, "Thursday");
+            weekdays.Add(5, "Friday");
+            weekdays.Add(6, "Saturday");
+            weekdays.Add(7, "Sunday");
+
+            // Total entries in the dictionary
+            Console.WriteLine("There are {0} items in the dictionary currently", weekdays.Count);
+
+            // Check of an particular value
+            if (weekdays.ContainsValue("Friday"))
+            {
+                Console.WriteLine("\nFriday is the last week day!");
+                weekdays.Remove(6);
+                weekdays.Remove(7);
+            }
+
+            // Display the dictionary entries
+            foreach (KeyValuePair<int, string> items in weekdays)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", items.Key, items.Value);
+            }
+
+            // Accessing individual elements
+            Console.WriteLine("\nThe First day is " + weekdays[1]);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week 9 - RichTextBox, Collections and Generics/Week 9 Programs in Class/LinkeListDemo/LinkeListDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Week9CallProgram
+{
+    class LinkeListDemo
+    {
+        static void Main(string[] args)
+        {
+            // Create a LinkedList
+            LinkedList<string> weekdays = new LinkedList<string>();
+            
+            // Add data into the list
+            weekdays.AddFirst("Monday");
+            weekdays.AddLast("Saturday");
+
+            // Add "Tuesday" after "Monday"
+            LinkedListNode<string> current = weekdays.Find("Monday");
+            weekdays.AddAfter(current, "Tuesday");
+
+            current = weekdays.Find("Saturday");
+            weekdays.AddAfter(current, "Sunday");
+            weekdays.AddBefore(current, "Friday");
+
+            // Display items in LinkedList
+            foreach (string items in weekdays)
+            {
+                Console.WriteLine(items);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
