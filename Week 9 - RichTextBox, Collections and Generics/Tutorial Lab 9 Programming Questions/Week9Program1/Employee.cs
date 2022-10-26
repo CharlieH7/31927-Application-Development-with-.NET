aa6d2e8cc58a3bf6e09c6ba5c1273bd18a0a5ea5
Charlie Huang <hope91824@gmail.com>
2022-10-26 18:21:17 +1100

    lab 9

---
+++ Week 9 - RichTextBox, Collections and Generics/Tutorial Lab 9 Programming Questions/Week9Program1/Employee.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Week9Program1;
+
+namespace Week9Program1
+{
+    class Employee : IComparable<Employee>
+    {
+        // Write code for the Properties of an Employee
+        // Name (First and last name), ID#, hourly rate and total working hours per week
+        string employeeFirstName { get; set; }
+        string employeeLastName { get; set; }
+        double hourlyRate { get; set; }
+        double workHours { get; set; }
+        int employeeID { get; set; }
+
+        // Write code for the Empolyee Constructor
+        public Employee()
+        {
+            employeeFirstName = "";
+            employeeLastName = "";
+            hourlyRate = 0.0;
+            workHours = 0.0;
+            employeeID = 0;
+        }
+
+        // Write code for the Method to recieve a single line from the emp.txt file as string
+        // and extract individual fields from the line by spltting the line using ','
+        public void LoadEmployee(string fileLine)
+        {
+            // Split the comma seperated string into fields 
+            string[] fields = fileLine.Split(',');
+                       
+            // Assign values to respective properties/ members
+            employeeLastName = fields[0];
+            employeeFirstName = fields[1];
+            hourlyRate = Convert.ToDouble(fields[2]);
+            employeeID = Convert.ToInt32(fields[3]);
+            workHours = Convert.ToDouble(fields[4]);
+        }
+
+        // Method to calculate the weekly salary of an Employee
+        public double GetWeeklySal()
+        {
+            return (workHours * hourlyRate);
+        }
+
+        // Overide the ToString() method to display the employee details
+        public override string ToString()
+        {
+            return employeeFirstName + " " + employeeLastName + " ID#:" + employeeID + " Weekly Income:" + GetWeeklySal();
+        }
+
+        // Write code for the Implemention of the CompareTo method 
+        public int CompareTo(Employee other)
+        {
+            if (employeeID < other.employeeID)
+            {
+                return -1;
+            }
+            else if (employeeID == other.employeeID)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+    }
+}
---
+++ Week 9 - RichTextBox, Collections and Generics/Tutorial Lab 9 Programming Questions/Week9Program1/EmployeeList.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week9Program1
+{
+    class EmployeeList
+    {
+        // Write code for to Create a List to store the information from emp.txt file
+        List<Employee> employees;
+
+        // EmployeeList constructor
+        public EmployeeList()
+        {
+            // Create an instance of List<>
+            employees = new List<Employee>();
+        }
+
+        // Method to process a string which contain a single line for the emp.txt
+        // The lfile name is passed as a parameter to the Method
+        public void LoadEmployees(string filename)
+        {
+            // Read the file content using the StreamReader
+            StreamReader fileContent = new StreamReader(filename);
+            // Create an object of Employee class
+            Employee tempEmployee;
+
+            // Read the StremReader till the last line
+            while (!fileContent.EndOfStream)
+            {
+                // At this point there are still employees to be loaded
+                tempEmployee = new Employee();
+                // Write code to Read each line and from the StreamReader
+                string line = fileContent.ReadLine();
+                // Write code to Load the employee detail from file to respective fields
+                tempEmployee.LoadEmployee(line);
+                // Write code to Add the details to the list collection
+                employees.Add(tempEmployee);
+            }
+            // Write code to Close the StreamReader
+            fileContent.Close();
+        }
+
+        // Method to display the details for all the Employee
+        public void PrintEmployees()
+        {
+            // prints each employee details in the employees list on a seperate line
+            foreach (Employee e in employees)
+            {
+                Console.WriteLine("Employee Details: " + e.ToString());
+            }
+
+        }
+        //  Write code to Sort the employee details based on the Employee ID
+        public void SortEmployees()
+        {
+            employees.Sort();
+        }
+    }
+}
