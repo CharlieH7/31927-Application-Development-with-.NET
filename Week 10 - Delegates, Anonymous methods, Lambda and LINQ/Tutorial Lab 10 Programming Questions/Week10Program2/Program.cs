6b863d02be12081dd5334c2298b8482f704560d0
Charlie Huang <hope91824@gmail.com>
2022-11-23 17:43:06 +1100

    Add files via upload

---
+++ Week 10 - Delegates, Anonymous methods, Lambda and LINQ/Tutorial Lab 10 Programming Questions/Week10Program2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+/*
+Write a program to count the number of files with similar extension and group them using LINQ. 
+Use lambda expressions to form the query.
+
+Hint:
+Trim and  ToLower method can be useful
+*/
+
+namespace Week10Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a List of files with different extensions
+            string[] fileList = { "aaa.txt", "bbb.TXT", "xyzabc.pdf", "hello.PDF", "abc.xml", "ccc.txt", "zzz.txt" };
+
+            // Create a Lambda expression for selection of file extension
+            var egrp = fileList.Select(file => Path.GetExtension(file).TrimStart('.').ToLower())
+
+                     .GroupBy(x => x, (ext, extCnt) => new
+
+                     {
+
+                         Extension = ext,
+
+                         Count = extCnt.Count()
+
+                     });
+
+            // Count each extension type and display the result. Use foreach
+            foreach (var v in egrp)
+            {
+                Console.WriteLine("{0} Files(s) with {1} Extension ", v.Count, v.Extension);
+            }
+            Console.ReadLine();
+
+        }
+    }
+
+}
+
+
+/*
+Test case: Expected output 
+
+4 File(s) with txt Extension
+2 File(s) with pdf Extension
+1 File(s) with xml Extension
+
+    */
